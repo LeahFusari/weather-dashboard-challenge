d2b5ebbf31e3646fe5771d20fbcc7e9df869e071
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-11-01 14:06:30 -0400

    upated HTML to accept dynamically created elements.  Wrote psudocode

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,14 @@
-var ApiKey = "443ca5186866a70d54e25c4442078beb"
+const apiKey = "443ca5186866a70d54e25c4442078beb"
+var city = document.getElementById("#iptCurrCity")
+
+
+// var requestURL = "https://api.openweathermap.org/data/2.5/weather?zip=01088&appid=443ca5186866a70d54e25c4442078beb"
+
+// fetch(requestURL)
+// .then(function(response){
+//     return response.json();
+// })
+
+// .then (function (data){
+//     console.log(data);
+// });

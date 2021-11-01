ba32c42aea99f96cb5c11faafced5e0be1d14141
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-11-01 16:38:03 -0400

    got first fetch to work and display in current weather, on click

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,13 +2,28 @@ const apiKey = "443ca5186866a70d54e25c4442078beb"
 var city = document.getElementById("#iptCurrCity")
 
 
-// var requestURL = "https://api.openweathermap.org/data/2.5/weather?zip=01088&appid=443ca5186866a70d54e25c4442078beb"
+var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=Springfield,MA,us&units=imperial&appid=443ca5186866a70d54e25c4442078beb"
 
-// fetch(requestURL)
-// .then(function(response){
-//     return response.json();
-// })
 
-// .then (function (data){
-//     console.log(data);
-// });
+$("#get-weather").click(function(){
+    fetch(requestURL)
+
+    .then(function(response){
+        return response.json();
+    })
+
+    .then(function(response){
+        var currWethEl= document.querySelector("#curr-weather-ul");
+        currWethEl.innerHTML=" ";
+        var currTemp=document.createElement("li");
+        var currWind=document.createElement("li");
+            
+        currTemp.textContent= "Current Tempreture: "+ response.main.temp + "\u00B0 F";
+        currWind.textContent= "Current Wind: "+ response.wind.speed + " MPH";
+
+        currWethEl.appendChild(currTemp);
+        currWethEl.appendChild(currWind);
+        console.log(response);
+        console.log(currWind);
+    })
+})

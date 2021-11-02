00ff4f9cb248f77600d4deaa89cb58c6271fda74
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-11-02 15:47:50 -0400

    refactored a little

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 const apiKey = "443ca5186866a70d54e25c4442078beb"
-
+var city = $("#input-city").val();
 
 
 
@@ -9,11 +9,15 @@ const apiKey = "443ca5186866a70d54e25c4442078beb"
 
 
 $("#get-weather").click(function(){
-  var city = $("#input-city").val();
-  var request1URL = "https://api.openweathermap.org/data/2.5/weather?q="+ city + ",us&units=imperial&appid=" + apiKey;
-//   var request2URL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&units=imperial&appid=" + apiKey;
+    displayCurrent()
+  //   fiveDayForecast()
+})
+
+function displayCurrent(){
+   
+    var request1URL = "https://api.openweathermap.org/data/2.5/weather?q="+ city + ",us&units=imperial&appid=" + apiKey;
   
-  fetch(request1URL)
+    fetch(request1URL)
 
     .then(function(response1){
         return response1.json();
@@ -24,14 +28,11 @@ $("#get-weather").click(function(){
         var lat=response1.coord.lat;
         var long=response1.coord.lon;
         var request2URL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + apiKey;
-        console.log(lat);
-        console.log(long);
         
         currWethEl.innerHTML=" ";
         var currTemp=document.createElement("li");
         var currWind=document.createElement("li");
         var currHumid=document.createElement("li");
-        // var currUVI=document.createElement("li");
             
         currTemp.textContent= "Current Tempreture: "+ response1.main.temp + "\u00B0 F";
         currWind.textContent= "Current Wind: "+ response1.wind.speed + " MPH";
@@ -40,7 +41,7 @@ $("#get-weather").click(function(){
         currWethEl.appendChild(currTemp);
         currWethEl.appendChild(currWind);
         currWethEl.appendChild(currHumid);
-        console.log(response1);
+        // console.log(response1);
 
         return fetch(request2URL)
 
@@ -48,15 +49,12 @@ $("#get-weather").click(function(){
             return response2.json();
           })
         
-          .then(function(response2){
-            var currUVI=document.createElement("li");
-
-            currUVI.textContent= "Current UV Index: "+ response2.current.uvi;
-
-            currWethEl.appendChild(currUVI);
-          console.log(response2);
-          })
-        
+        .then(function(response2){
+        var currUVI=document.createElement("li");
+        currUVI.textContent= "Current UV Index: "+ response2.current.uvi;
+        currWethEl.appendChild(currUVI);
+        // console.log(response2);
+        })
     })
+}
 
-})
